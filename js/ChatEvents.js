71c9990f8a34daa169b95386dfee2facddadd03d
Muhammed Ali Özyiğit <87724680+Woikz0@users.noreply.github.com>
2025-10-02 20:52:35 +0300

    Change Obama GIF URL to local file

--- js/ChatEvents.js
+++ js/ChatEvents.js
@@ -6,7 +6,7 @@ function checkWord(word) {
             document.getElementById("chat-area").style.backgroundImage = "url('wow.gif')";
             break;
         case "obama":
-            document.getElementById("chat-area").style.backgroundImage = "url('https://media.tenor.com/lOVqHrlKsT0AAAAj/obama-prism.gif')";
+            document.getElementById("chat-area").style.backgroundImage = "url('obama-prism.gif')";
             break;
         case "owye":
             document.getElementById("chat-area").style.backgroundImage = "url('anim.gif')";
